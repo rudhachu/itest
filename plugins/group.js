912166829387220c672e5be995d7cd86c0d16734
rudhachu <120007671+rudhachu@users.noreply.github.com>
2025-03-20 16:55:49 +0530

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -14,58 +14,22 @@ plugin({
 	onlyGroup: true,
 	desc: 'promote group member'
 }, async (message, match) => {
-	if (!await isBotAdmin(message)) return await message.send('_bot must be admin first_', {
-		linkPreview: linkPreview()
-	})
-	if (!message.send_message?.sender) return message.send('_please reply to a user_', {
-		linkPreview: linkPreview()
-	})
-	await message.client.groupParticipantsUpdate(message.jid,
-		[message.send_message?.sender], "promote");
-	message.send(`_@${message.send_message?.sender.split('@')[0]} promoted as admin successfully_`, {
-		mentions: [message.send_message?.sender],
-		linkPreview: linkPreview()
-	})
-});
-plugin({
-	pattern: 'kick ?(.*)',
-	type: 'group',
-	fromMe: true,
-	onlyGroup: true,
-	desc: "kick group member'"
-}, async (message, match) => {
 	let admin = await isAdmin(message);
-	let user = message.send_message?.sender || match;
-	if (!user) return await message.send('_please reply to user_', {
+	let BotAdmin = await isBotAdmin(message);
+	if (!BotAdmin) return await message.send('*Bot must be admin first*', {
 		linkPreview: linkPreview()
 	})
-	user = user.replace(/[^0-9]/g, "") + "@s.whatsapp.net";
-	if (match != "all") {
-		if (!await isBotAdmin(message)) return await message.send('_bot must be admin first_', {
-			linkPreview: linkPreview()
-		})
-		await message.client.groupParticipantsUpdate(message.jid,
-			[user], "remove");
-		return await message.send(`_@${user.split('@')[0]} kick from group_`, {
-			mentions: [user],
-			linkPreview: linkPreview()
-		});
-	} else if (match.toLowerCase() == 'all') {
-		if (!await isBotAdmin(message)) return await message.send('_bot must be admin first_', {
-			linkPreview: linkPreview()
-		})
-		const groupMetadata = await message.client.groupMetadata(message.jid).catch(e => {})
-		const participants = await groupMetadata.participants;
-		let admins = await participants.filter(v => v.admin !== null).map(v => v.id);
-		participants.filter((U) => !U.admin == true).map(({
-			id
-		}) => id).forEach(async (k) => {
-			await sleep(2500);
-			await message.client.groupParticipantsUpdate(message.jid,
-				[k], "remove");
-		});
-		return await message.send('all group Participants will been kicked!')
-	}
+	if (!config.ADMIN_ACCESS && !message.isCreator) return;
+	if (!admin && !message.isCreator) return;
+	if (!message.reply_message.sender) return message.send('*Please reply to a user*', {
+		linkPreview: linkPreview()
+	});
+	await message.client.groupParticipantsUpdate(message.jid,
+		[message.reply_message.sender], "promote");
+	message.send(`*@${message.reply_message.sender.split('@')[0]} Promoted as admin*`, {
+		mentions: [message.reply_message.sender],
+		linkPreview: linkPreview()
+	})
 });
 
 plugin({
@@ -73,23 +37,71 @@ plugin({
 	type: 'group',
 	fromMe: true,
 	onlyGroup: true,
-	desc: "demote group member"
+	desc: "Demote group member"
 }, async (message, match) => {
 	let admin = await isAdmin(message);
-	if (!await isBotAdmin(message)) return await message.send('_bot must be admin first_', {
+	let BotAdmin = await isBotAdmin(message);
+	if (!BotAdmin) return await message.send('*Bot must be admin first*', {
 		linkPreview: linkPreview()
 	})
-	if (!message.send_message?.sender) return message.send('_please reply to a user_', {
+	if (!config.ADMIN_ACCESS && !message.isCreator) return;
+	if (!admin && !message.isCreator) return;
+	if (!message.reply_message.sender) return message.send('*Please reply to a user*', {
 		linkPreview: linkPreview()
 	});
 	await message.client.groupParticipantsUpdate(message.jid,
-		[message.send_message?.sender], "demote");
-	return await message.send(`_@${message.send_message?.sender.split('@')[0]} demoted from admin successfully_`, {
-		mentions: [message.send_message?.sender],
+		[message.reply_message.sender], "demote");
+	return await message.send(`*@${message.reply_message.sender.split('@')[0]} Demoted From Admin*`, {
+		mentions: [message.reply_message.sender],
 		linkPreview: linkPreview()
 	})
 });
 
+plugin({
+	pattern: 'kick ?(.*)',
+	type: 'group',
+	fromMe: true,
+	onlyGroup: true,
+	desc: "kick group member"
+}, async (message, match) => {
+	let admin = await isAdmin(message);
+	let BotAdmin = await isBotAdmin(message);
+	let user = message.send_message?.sender || match;
+	if (!user) return await message.send('*Please reply to a user*', {
+		linkPreview: linkPreview()
+	})
+	user = user.replace(/[^0-9]/g, "") + "@s.whatsapp.net";
+	if (match != "all") {
+		if (!BotAdmin) return await message.send('*Bot must be admin first*', {
+		linkPreview: linkPreview()
+	})
+		if (!config.ADMIN_ACCESS && !message.isCreator) return;
+		if (!admin && !message.isCreator) return;
+		await message.client.groupParticipantsUpdate(message.jid,
+			[user], "remove");
+		return await message.send(`*@${user.split('@')[0]} kick from group*`, {
+			mentions: [user],
+			linkPreview: linkPreview()
+		});
+	} else if (match.toLowerCase() == 'all') {
+		if (!BotAdmin) return await message.send('*Bot must be admin first*', {
+		linkPreview: linkPreview()
+	})
+		if (!config.ADMIN_ACCESS && !message.isCreator) return;
+		if (!admin && !message.isCreator) return;
+		const groupMetadata = await message.client.groupMetadata(message.jid).catch(e => {})
+		const participants = await groupMetadata.participants;
+		let admins = await participants.filter(v => v.admin !== null).map(v => v.id);
+		participants.filter((U) => !U.admin == true).map(({
+			id
+		}) => id).forEach(async (k) => {
+			await sleep(250);
+			await message.client.groupParticipantsUpdate(message.jid,
+				[k], "remove");
+		});
+		return await message.reply('_All group Participants will been kicked!_')
+	}
+});
 
 plugin({
 	pattern: 'revoke ?(.*)',
@@ -98,11 +110,15 @@ plugin({
 	onlyGroup: true,
 	desc: 'revoke group link'
 }, async (message, match) => {
-	if (!await isBotAdmin(message)) return await message.send('_bot must be admin first_', {
+	const BotAdmin = await isBotAdmin(message);
+	const admin = await isAdmin(message);
+	if (!BotAdmin) return await message.send('*Bot must be admin first*', {
 		linkPreview: linkPreview()
 	})
+	if (!config.ADMIN_ACCESS && !message.isCreator) return;
+	if (!admin && !message.isCreator) return;
 	await message.client.groupRevokeInvite(message.jid);
-	return await message.send('_successfully revoked group link_', {
+	return await message.send('*Successfully revoked group link*', {
 		linkPreview: linkPreview()
 	})
 });
@@ -130,13 +146,33 @@ plugin({
 	onlyGroup: true,
 	desc: 'change group privacy to only admins edit'
 }, async (message, match) => {
-	if (!await isBotAdmin(message)) return await message.send('_bot must be admin first_', {
+	const BotAdmin = await isBotAdmin(message);
+	const admin = await isAdmin(message);
+	if (!BotAdmin) return await message.send('*Bot must be admin first*', {
 		linkPreview: linkPreview()
 	})
-	await message.client.groupSettingUpdate(message.jid, 'locked');
-	return await message.send('_successfully changed group settings_', {
+	if (!config.ADMIN_ACCESS && !message.isCreator) return;
+	if (!admin && !message.isCreator) return;
+	await message.client.groupSettingUpdate(message.jid, "locked");
+	return await message.reply('*Group Settings Locked*')
+});
+
+plugin({
+	pattern: 'unlock ?(.*)',
+	type: 'group',
+	fromMe: true,
+	onlyGroup: true,
+	desc: 'change group privacy to members can edit'
+}, async (message, match) => {
+	const BotAdmin = await isBotAdmin(message);
+	const admin = await isAdmin(message);
+	if (!BotAdmin) return await message.send('*Bot must be admin first*', {
 		linkPreview: linkPreview()
 	})
+	if (!config.ADMIN_ACCESS && !message.isCreator) return;
+	if (!admin && !message.isCreator) return;
+	await message.client.groupSettingUpdate(message.jid, "unlocked");
+	return await message.reply('*Group Settings Unlocked*')
 });
 
 plugin({
@@ -144,13 +180,17 @@ plugin({
 	type: 'group',
 	fromMe: true,
 	onlyGroup: true,
-	desc: 'mute group'
+	desc: 'change group privacy to only admins edit'
 }, async (message, match) => {
-	if (!await isBotAdmin(message)) return await message.send('_bot must be admin first_', {
+	const BotAdmin = await isBotAdmin(message);
+	const admin = await isAdmin(message);
+	if (!BotAdmin) return await message.send('*Bot must be admin first*', {
 		linkPreview: linkPreview()
 	})
-	await message.client.groupSettingUpdate(message.jid, 'announcement');
-	return await message.send('_group muted_', {
+	if (!config.ADMIN_ACCESS && !message.isCreator) return;
+	if (!admin && !message.isCreator) return;
+	await message.client.groupSettingUpdate(message.jid, "announcement");
+	return await message.send('*Group Muted!*', {
 		linkPreview: linkPreview()
 	})
 });
@@ -162,11 +202,15 @@ plugin({
 	onlyGroup: true,
 	desc: 'unmute a group'
 }, async (message, match) => {
-	if (!await isBotAdmin(message)) return await message.send('_bot must be admin first_', {
+	const BotAdmin = await isBotAdmin(message);
+	const admin = await isAdmin(message);
+	if (!BotAdmin) return await message.send('*Bot must be admin first*', {
 		linkPreview: linkPreview()
 	})
-	await message.client.groupSettingUpdate(message.jid, 'not_announcement');
-	return await message.send('_group unmuted_', {
+	if (!config.ADMIN_ACCESS && !message.isCreator) return;
+	if (!admin && !message.isCreator) return;
+	await message.client.groupSettingUpdate(message.jid, "not_announcement");
+	return await message.send('*Group Unmuted!*', {
 		linkPreview: linkPreview()
 	})
 });
@@ -178,31 +222,44 @@ plugin({
 	onlyGroup: true,
 	desc: 'update group description'
 }, async (message, match) => {
-	if (!await isBotAdmin(message)) return await message.send('_bot must be admin first_', {
+	const BotAdmin = await isBotAdmin(message);
+	const admin = await isAdmin(message);
+	if (!BotAdmin) return await message.send('*Bot must be admin first*', {
 		linkPreview: linkPreview()
 	})
-	if (message.text > 400) return await message.send('_can`t be updated_', {
+	if (!config.ADMIN_ACCESS && !message.isCreator) return;
+	if (!admin && !message.isCreator) return;
+	if (message.text > 400) return await message.send('*Can`t be updated*', {
 		linkPreview: linkPreview()
 	})
-	let txt = match || ' ';
+	let txt = match || " ";
 	await message.client.groupUpdateDescription(message.jid, txt);
-	return await message.send('_updated successfully_', {
+	return await message.send('*Updated successfully*', {
 		linkPreview: linkPreview()
 	})
 });
 
+
 plugin({
-	pattern: 'unlock ?(.*)',
+	pattern: 'gname ?(.*)',
 	type: 'group',
 	fromMe: true,
 	onlyGroup: true,
-	desc: 'chamge group privacy to members can edit'
+	desc: 'Update group name'
 }, async (message, match) => {
-	if (!await isBotAdmin(message)) return await message.send('_bot must be admin first_', {
+	const BotAdmin = await isBotAdmin(message);
+	const admin = await isAdmin(message);
+	if (!BotAdmin) return await message.send('*Bot must be admin first*', {
 		linkPreview: linkPreview()
 	})
-	await message.client.groupSettingUpdate(message.jid, 'unlocked');
-	return await message.send('_successfully changed group settings_', {
+	if (!config.ADMIN_ACCESS && !message.isCreator) return;
+	if (!admin && !message.isCreator) return;
+	if (message.text > 75) return await message.send('*Can`t be updated*', {
+		linkPreview: linkPreview()
+	})
+	let txt = message.text || " ";
+	await message.client.groupUpdateSubject(message.jid, txt);
+	return await message.send('*Group name updated*', {
 		linkPreview: linkPreview()
 	})
 });
@@ -218,39 +275,24 @@ plugin({
 });
 
 plugin({
-	pattern: 'gname ?(.*)',
-	type: 'group',
-	fromMe: true,
-	onlyGroup: true,
-	desc: 'update group name'
-}, async (message, match) => {
-	if (!await isBotAdmin(message)) return await message.send('_bot must be admin first_', {
-		linkPreview: linkPreview()
-	})
-	if (message.text > 75) return await message.send('_can`t be updated_', {
-		linkPreview: linkPreview()
-	})
-	let txt = message.text || ' ';
-	await message.client.groupUpdateSubject(message.jid, txt);
-	return await message.send('_group name updated_', {
-		linkPreview: linkPreview()
-	})
-});
-
-plugin({
 	pattern: 'gpp ?(.*)',
 	type: 'group',
 	fromMe: true,
 	onlyGroup: true,
-	desc: 'update group icon'
+	desc: 'Update group icon'
 }, async (message, match) => {
-	if (!await isBotAdmin(message)) return await message.send('_bot must be admin first_', {
+	const BotAdmin = await isBotAdmin(message);
+	const admin = await isAdmin(message);
+	if (!BotAdmin) return await message.send('*Bot must be admin first*', {
 		linkPreview: linkPreview()
 	})
-	if (!message.reply_message.image) return await message.send('__please reply to a image message_');
-	let download = await message.client.downloadMediaMessage(message.reply_message.imageMessage);
+	if (!config.ADMIN_ACCESS && !message.isCreator) return;
+	if (!admin && !message.isCreator) return;
+	if (!message.reply_message.image) return await message.send('*Please reply to a image message*');
+	let _message = message.reply_message.imageMessage;
+	let download = await message.client.downloadMediaMessage(_message);
 	await message.client.updateProfilePicture(message.jid, download);
-	return message.send('_Group profile updated_', {
+	return message.send('*Group profile updated*', {
 		linkPreview: linkPreview()
 	})
 })
@@ -260,14 +302,19 @@ plugin({
 	type: 'group',
 	fromMe: true,
 	onlyGroup: true,
-	desc: 'update group icon'
+	desc: 'Update group icon'
 }, async (message, match) => {
-	if (!await isBotAdmin(message)) return await message.send('_bot must be admin first_', {
+	const BotAdmin = await isBotAdmin(message);
+	const admin = await isAdmin(message);
+	if (!BotAdmin) return await message.send('*Bot must be admin first*', {
 		linkPreview: linkPreview()
 	})
-	if (!message.reply_message.image) return await message.send('_please reply to a image message_');
-	await message.updateProfilePicture(message.jid, await message.reply_message.download());
-	return message.send('_profile photo updated_', {
+	if (!config.ADMIN_ACCESS && !message.isCreator) return;
+	if (!admin && !message.isCreator) return;
+	if (!message.reply_message.image) return await message.send('*Please reply to a image message*');
+	let download = await message.reply_message.download();
+	await message.updateProfilePicture(message.jid, download);
+	return message.send('*Profile photo updated*', {
 		linkPreview: linkPreview()
 	})
 });
@@ -278,15 +325,15 @@ plugin({
 	fromMe: true,
 	desc: 'join a group using group link'
 }, async (message, match) => {
-	if (!match || !match.match(/^https:\/\/chat\.whatsapp\.com\/[a-zA-Z0-9]/)) return await message.send('_invalid url_', {
+	if (!match || !match.match(/^https:\/\/chat\.whatsapp\.com\/[a-zA-Z0-9]/)) return await message.send('*invalid url*', {
 		linkPreview: linkPreview()
 	});
 	let urlArray = (match).trim().split('/');
-	if (!urlArray[2] == 'chat.whatsapp.com') return await message.send('_url must be a whatsapp group link_', {
+	if (!urlArray[2] == 'chat.whatsapp.com') return await message.send('Url must be a whatsapp group link*', {
 		linkPreview: linkPreview()
 	})
 	const response = await message.client.groupAcceptInvite(urlArray[3]);
-	return await message.send('_successfully joind_', {
+	return await message.send('*Successfully joind!*', {
 		linkPreview: linkPreview()
 	})
 });
@@ -298,46 +345,48 @@ plugin({
 	onlyGroup: true,
 	desc: "add member's to group"
 }, async (message, match) => {
-	match = message.send_message?.sender || match;
-	if (!match) return await message.send('_please reply to a user_');
+	const BotAdmin = await isBotAdmin(message);
+	const admin = await isAdmin(message);
+	match = message.reply_message.sender || match;
+	if (!match) return await message.send('*Please reply to a user*');
 	match = match.replaceAll(' ', '');
-	if (!await isBotAdmin(message)) return await message.send('_bot must be admin first_', {
+	if (!BotAdmin) return await message.send('*Bot must be admin first*', {
 		linkPreview: linkPreview()
 	})
+	if (!config.ADMIN_ACCESS && !message.isCreator) return;
+	if (!admin && !message.isCreator) return;
 	if (match) {
 		let users = match.replace(/[^0-9]/g, '') + '@s.whatsapp.net';
 		let info = await message.client.onWhatsApp(users);
 		ex = info.map((jid) => jid.jid);
-		if (!ex.includes(users)) return await message.send('user not in whatsapp');
+		if (!ex.includes(users)) return await message.send('*User not in whatsapp*');
 		const su = await message.client.groupParticipantsUpdate(message.jid,
 			[users], "add");
 		if (su[0].status == 403) {
-			await message.send('_Couldn\'t add. Invite sent!_');
+			message.send('*Couldn\'t add. Invite sent!*');
 			return await message.sendGroupInviteMessage(users);
 		} else if (su[0].status == 408) {
-			await message.send("Couldn\'t add because they left the group recently. Try again later.", {
+			await message.send("*Couldn\'t add because they left the group recently. Try again later.*", {
 				linkPreview: linkPreview()
 			})
 			const code = await message.client.groupInviteCode(message.jid);
-			return await message.client.sendMessage(users, {
-				text: `https://chat.whatsapp.com/${code}`
-			})
+			return await message.client.sendMessage(users, {text: `https://chat.whatsapp.com/${code}`})
 		} else if (su[0].status == 401) {
-			await message.send('Couldn\'t add because they blocked the bot number.', {
+			await message.send('*Couldn\'t add because they blocked the bot number.*', {
 				linkPreview: linkPreview()
 			})
 		} else if (su[0].status == 200) {
-			return await message.send(`@${users.split('@')[0]} Added to the group.`, {
+			return await message.send(`*@${users.split('@')[0]} Added to the group.*`, {
 				mentions: [users],
 				linkPreview: linkPreview()
 			})
 		} else if (su[0].status == 409) {
-			return await message.send("Already in the group.", {
+			return await message.send("*Already in the group.*", {
 				mentions: [users],
 				linkPreview: linkPreview()
 			})
 		} else {
-			return await message.send(su);
+			return await message.send(JSON.stringify(su));
 		}
 	}
 });
